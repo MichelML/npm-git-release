07aed14760dcba8970afc46972af6c00cabe5947
MichelML <michmoreau.l@gmail.com>
2018-02-24 13:05:20 -0500

    remove out from github

--- out/hellocljs/utils.cljs
+++
@@ -1,3 +0,0 @@
-(ns hellocljs.utils)
-
-(defn ouone [] (print 1))
--- out/hellocljs/utils.js
+++
@@ -1,8 +0,0 @@
-// Compiled by ClojureScript 1.9.946 {:target :nodejs}
-goog.provide('hellocljs.utils');
-goog.require('cljs.core');
-hellocljs.utils.ouone = (function hellocljs$utils$ouone(){
-return cljs.core.print.call(null,(1));
-});
-
-//# sourceMappingURL=utils.js.map
