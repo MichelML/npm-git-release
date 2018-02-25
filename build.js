b4b3c49659cd7df17de49d2c845de4027593f18b
MichelML <michmoreau.l@gmail.com>
2018-02-25 11:37:22 -0500

    add repl prefix on build

--- build.js
+++ build.js
@@ -3,6 +3,7 @@ const {execSync} = require('child_process')
 
 const lastArg = process.argv[process.argv.length - 1].trim().toLowerCase()
 const mode = ["--watch", "--repl"].includes(lastArg) ? ` ${lastArg}` : ""
+const replPrefix = ["--repl"].includes(lastArg) ? "rlwrap " : ""
 
-execSync(`java -cp ~/cljs.jar:src clojure.main conf.clj${mode}`, {stdio: 'inherit', shell: true})
+execSync(`${replPrefix}java -cp ~/cljs.jar:src clojure.main conf.clj${mode}`, {stdio: 'inherit', shell: true})
 
